c6d385fce0d0c85dcd5c938c2d26a113362abaa9
ShiroKazane <kaguyakizuto@gmail.com>
2024-10-31 23:02:07 +0900

    fix: adding content to koban page

--- src/client/ui/components/pages/koban.tsx
+++ src/client/ui/components/pages/koban.tsx
@@ -3,7 +3,7 @@ import React from "@rbxts/react";
 import { store } from "client/store";
 import { useRem, useTheme } from "client/ui/hooks";
 
-import { Button, Frame, TextLabel } from "../primitive";
+import { Button, Frame, ScrollingFrame, TextLabel } from "../primitive";
 
 export function KobanApp(): React.ReactNode {
 	const rem = useRem();
@@ -29,6 +29,43 @@ export function KobanApp(): React.ReactNode {
 				TextColor={theme.colors.text.primary}
 				TextSize={rem(1.5)}
 			/>
+			<ScrollingFrame
+				key="Content"
+				CanvasSize={new Vector2(0, 0)}
+				Native={{
+					AnchorPoint: new Vector2(0.5, 1),
+					CanvasSize: new UDim2(0, 0, 1, 0),
+					Position: new UDim2(0.5, 0, 0.985, 0),
+					ScrollBarThickness: 0,
+					Size: new UDim2(0.975, 0, 0.85, 0),
+				}}
+			>
+				<TextLabel
+					FontFace={theme.fonts.specialElite.regular}
+					Native={{
+						AnchorPoint: new Vector2(0.5, 0),
+						BackgroundTransparency: 1,
+						Position: new UDim2(0.5, 0, 0.02, 0),
+						Size: new UDim2(0.925, 0, 0, rem(3.125)),
+						TextXAlignment: "Left",
+						TextYAlignment: "Top",
+					}}
+					Text="try scrolling maybe?"
+					TextColor={theme.colors.text.primary}
+				/>
+				<TextLabel
+					FontFace={theme.fonts.specialElite.regular}
+					Native={{
+						AnchorPoint: new Vector2(0.5, 1),
+						BackgroundTransparency: 1,
+						Position: new UDim2(0.5, 0, 0.99, 0),
+						Size: new UDim2(0, rem(7.5), 0, rem(1.25)),
+					}}
+					Text="sadly nothin here"
+					TextColor={theme.colors.text.primary}
+					TextSize={rem(0.75)}
+				/>
+			</ScrollingFrame>
 			<Frame
 				key="Close"
 				Native={{
